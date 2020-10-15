2e0cea0013bb380874a690a0f0f951616693aa17
Frost77011 <strongjared@gmail.com>
2020-10-15 16:53:33 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById("randomButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    let s = document.getElementById('selector');
+    let number = s.options[s.selectedIndex].value;
+    const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes/" + number;
+    console.log(url);
+    fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+        let results = "";
+        for (let i = 0; i< number; i++) {
+            results += "<p>" + json[i] + "</p>";
+        }
+        document.getElementById("randomQuotes").innerHTML = results;
+    });
+});
+
+document.getElementById("searchSubmit").addEventListener("click", function(event) {
+    event.preventDefault();
+    const value = document.getElementById("searchInput").value;
+    console.log(value);
+    const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes/search/" + value;
+    console.log(url);
+    fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+        let results = "";
+        for (let i = 0; i< json.length; i++) {
+            results += "<p>" + json[i] + "</p>";
+        }
+        document.getElementById("searchQuotes").innerHTML = results;
+    });
+});
